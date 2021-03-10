aed1da0e58777d01a133dd56708cdd8d0b91f103
unknown <nikola.neskovic99@gmail.com>
2021-03-10 09:51:17 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,21 +13,19 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent =
-            data.forecastData.weather_description +
-            " temperature: " +
-            data.forecastData.temperature;
-          //   console.log(data.location);
-          console.log(data.forecastData.weather_description);
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent =
+          data.forecastData.weather_description +
+          " temperature: " +
+          data.forecastData.temperature;
+        //   console.log(data.location);
+        console.log(data.forecastData.weather_description);
+      }
+    });
+  });
 });
